582775aec62fad91c9e0013c21b9489ade34d43c
Ilya Andreichenko <ilia.andrejchenko@gmail.com>
2023-04-11 11:12:41 +0300

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className='w-full h-full text-black bg-white dark:text-sup-50 dark:bg-sup-900 font-roboto'>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   )
 }
